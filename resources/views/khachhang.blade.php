f2254abf5c1a57512e13fc6da2bf0efecdaa8ed0
NVHQuoc <quoc0212@gmail.com>
2016-12-16 15:58:32 +0700

    trung gà v2

--- resources/views/khachhang.blade.php
+++ resources/views/khachhang.blade.php
@@ -10,6 +10,19 @@
 
 @stop
 
-ssssssssssss
+<div class="list-group">
+    <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">First List Group Item Heading</h4>
+        <p class="list-group-item-text">List Group Item Text</p>
+    </a>
+    <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Second List Group Item Heading</h4>
+        <p class="list-group-item-text">List Group Item Text</p>
+    </a>
+    <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Third List Group Item Heading</h4>
+        <p class="list-group-item-text">List Group Item Text</p>
+    </a>
+</div>
 
 @stop
